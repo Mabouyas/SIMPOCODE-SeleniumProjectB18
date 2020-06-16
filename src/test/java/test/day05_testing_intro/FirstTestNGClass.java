11ba59fc032c5d42ff0b0641528ea9f4bdfbbde7
Jean Michel Chauvet <Cybertek.QA@gmail.com>
2020-06-16 16:47:37 -0400

    Initial commit

---
+++ src/test/java/test/day05_testing_intro/FirstTestNGClass.java
@@ -0,0 +1,14 @@
+package test.day05_testing_intro;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGClass {
+    @Test
+    public void test1(){
+        System.out.println("Test is running..");
+    }
+    
+    public void test2(){
+        System.out.println("Test2 is running...");
+    }
+}
