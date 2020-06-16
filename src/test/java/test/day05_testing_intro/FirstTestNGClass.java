208a91e15e0285f7a2ddabe66bf1ddab8edf9122
Jean Michel Chauvet <Cybertek.QA@gmail.com>
2020-06-16 16:54:24 -0400

    nitial commit

--- src/test/java/test/day05_testing_intro/FirstTestNGClass.java
+++ src/test/java/test/day05_testing_intro/FirstTestNGClass.java
@@ -1,13 +1,20 @@
 package test.day05_testing_intro;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class FirstTestNGClass {
+    @BeforeMethod
+    public void setup() {
+        System.out.println("Before method running...");
+    }
+
     @Test
     public void test1(){
-        System.out.println("Test is running..");
+        System.out.println("Test is running...");
     }
-    
+
+    @Test
     public void test2(){
         System.out.println("Test2 is running...");
     }
